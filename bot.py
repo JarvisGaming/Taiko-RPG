fb408c0ba5122d7ab5a6210402b78af8c6a86417
JarvisGaming <jarvisgaming40@gmail.com>
2024-06-16 17:14:46 +0800

    Moved todo list and notes to a separate file on my pc

--- bot.py
+++ bot.py
@@ -32,31 +32,3 @@ async def setup_hook():
 
 sys.stderr = open("./logs.log", "w")  # Redirect stderr to log file
 bot.run(BOT_TOKEN)
-
-"""
-- check for lazer mods (see pinned discord DM)
-    - check for missing life graph in replays, then prompt for the user to use a /submit command to submit the lazer replay
-    - use HTTP requests to get the (recent) score(s) with https://osu.ppy.sh/docs/index.html#get-user-scores (+ parameters maybe)
-    - turn the score(s) into an osrparse replay file(s)
-    - separate out the replay processing process from the original submit into a separate function
-- allow DT/HT (beatmap_attributes)
-- add secret command that responds with random messages
-- pagination: https://stackoverflow.com/questions/76247812/how-to-create-pagination-embed-menu-in-discord-py
-- online hosting (or a fucking raspberry pi)
-"""
-
-"""
-aiosqlite jank:
-- conetext manager closes the connection and cursor automatically, but doesn't auto commit??? fuck you
-"""
-
-"""
-Error handling order:
-1. Command-specific handler ({command}_error)
-2. Cog-specific handler (cog_command_error)
-3. Generic handler (eg on_command_error)
-
-ALL of them will be called. Use hasattr(ctx.cog, <name>) to determine if the command has a local / cog error handler.
-Cog: Check for command-specific handler.
-Generic: Check for command-specific and cog-specific handler.
-"""
