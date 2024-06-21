fcdedaaa2c2deabc7971de676ea11a76f6e9ff9f
JarvisGaming <jarvisgaming40@gmail.com>
2024-06-21 19:33:27 +0800

    Fix 727 check not ignoring pings

--- cogs/secrets.py
+++ cogs/secrets.py
@@ -25,13 +25,15 @@ class SecretsCog(commands.Cog):
     async def wysi(self, message: discord.Message):
         """Sends a message when a new message contains the number 727: https://knowyourmeme.com/memes/727-wysi"""
         
+        message_without_pings = message.content
+        
         # Removes the <@xxx> part(s) in the message, which are pings
         # https://regex-vis.com/?r=%3C%40%5B0-9%5D%2B%3F%3E
-        message_without_pings = re.sub("<@[0-9]+?>", "", message.content)
+        message_without_pings = re.sub("<@[0-9]+?>", "", message_without_pings)
         
         # Removes the :xxx: part(s) in the message, which are emojis
         # https://regex-vis.com/?r=%3A%5Cw%2B%3F%3A
-        message_without_pings = re.sub(r":\w+?:", "", message.content)
+        message_without_pings = re.sub(r":\w+?:", "", message_without_pings)
         
         if "727" in message_without_pings:
             channel = message.channel
