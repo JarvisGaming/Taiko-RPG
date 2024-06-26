396002bc3b76112255e2ae5520cf8c4ad40d1a38
JarvisGaming <jarvisgaming40@gmail.com>
2024-06-26 16:54:57 +0800

    check if interaction has already been responded to in error handing

--- other/error_handling.py
+++ other/error_handling.py
@@ -17,8 +17,12 @@ async def on_app_command_error(interaction: discord.Interaction, error: discord.
     Generic error handler for all slash commands.
     Note that both command-specific and cog-specific error handlers are called before this generic handler.
     """
-    
-    if isinstance(error, discord.app_commands.CommandOnCooldown):
+    if interaction.response.is_done():
+        original_response = await interaction.original_response()
+        await original_response.edit(content=f"An exception occurred: {error}")
+        
+    elif isinstance(error, discord.app_commands.CommandOnCooldown):
         await interaction.response.send_message(error)
+        
     else:
         await interaction.response.send_message(f"An exception occurred: {error}")
