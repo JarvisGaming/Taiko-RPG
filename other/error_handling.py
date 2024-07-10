e9426fb84196fe89c2ee81e98e5172624c95bb3d
JarvisGaming <jarvisgaming40@gmail.com>
2024-07-10 22:52:00 +0800

    Fixed forgetting to turn on error handling

--- other/error_handling.py
+++ other/error_handling.py
@@ -1,36 +1,37 @@
 from discord.ext import commands
 from other.global_constants import *
 
-# @bot.event
-# async def on_command_error(ctx: commands.Context, error):
-#     """
-#     Generic error handler for all text commands.
-#     Note that both command-specific and cog-specific error handlers are called before this generic handler.
-#     """
-    
-#     # Checks like is_verified are already handled
-#     if isinstance(error, commands.CheckFailure):
-#         return
-    
-#     await ctx.send(f"An exception occurred: {error}")
 
-# @bot.tree.error
-# async def on_app_command_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
-#     """
-#     Generic error handler for all slash commands.
-#     Note that both command-specific and cog-specific error handlers are called before this generic handler.
-#     """
+@bot.event
+async def on_command_error(ctx: commands.Context, error):
+    """
+    Generic error handler for all text commands.
+    Note that both command-specific and cog-specific error handlers are called before this generic handler.
+    """
     
-#     if isinstance(error, discord.app_commands.CommandOnCooldown):
-#         await interaction.response.send_message(error)
+    # Checks like is_verified are already handled
+    if isinstance(error, commands.CheckFailure):
+        return
     
-#     # Checks like is_verified are already handled
-#     elif isinstance(error, discord.app_commands.CheckFailure):
-#         return
+    await ctx.send(f"An exception occurred: {error}")
+
+@bot.tree.error
+async def on_app_command_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
+    """
+    Generic error handler for all slash commands.
+    Note that both command-specific and cog-specific error handlers are called before this generic handler.
+    """
+    
+    if isinstance(error, discord.app_commands.CommandOnCooldown):
+        await interaction.response.send_message(error)
+    
+    # Checks like is_verified are already handled
+    elif isinstance(error, discord.app_commands.CheckFailure):
+        return
         
-#     elif interaction.response.is_done():
-#         original_response = await interaction.original_response()
-#         await original_response.edit(content=f"An exception occurred: {error}")
+    elif interaction.response.is_done():
+        original_response = await interaction.original_response()
+        await original_response.edit(content=f"An exception occurred: {error}")
         
-#     else:
-#         await interaction.response.send_message(f"An exception occurred: {error}")
+    else:
+        await interaction.response.send_message(f"An exception occurred: {error}")
