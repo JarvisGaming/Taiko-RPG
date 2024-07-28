32071fead5e4860843e6f7d220642122f33000ea
JarvisGaming <jarvisgaming40@gmail.com>
2024-07-28 18:29:21 +0800

    Added new upgrades

--- classes/upgrade_manager.py
+++ classes/upgrade_manager.py
@@ -54,12 +54,88 @@ class UpgradeManager:
             effect_type = BuffEffectType.MULTIPLICATIVE,
             effect_impl = overall_exp_gain_multiplier_effect,
         ))
+        
+        def nm_exp_gain_multiplier_effect(upgrade_level: int, exp_bar_exp_gain: dict[str, int], user_exp_bars: dict[str, "ExpBar"]):
+            exp_bar_exp_gain['NM'] = int (exp_bar_exp_gain['NM'] * (1 + 0.002 * upgrade_level * user_exp_bars['NM'].level))
+        
+        self.upgrades['nm_exp_gain_multiplier'] = (Upgrade(
+            id = "nm_exp_gain_multiplier",
+            name = "NM EXP Gain Multiplier", 
+            description = "+0.2% NM EXP gain / NM level, per upgrade level (additive)",
+            max_level = 5,
+            cost_currency_unit = "taiko_tokens", 
+            cost = lambda level: 200 * (level ** 2),
+            effect = BuffEffect.NM_EXP_GAIN,
+            effect_type = BuffEffectType.MULTIPLICATIVE,
+            effect_impl = nm_exp_gain_multiplier_effect,
+        ))
+        
+        def hd_exp_gain_multiplier_effect(upgrade_level: int, exp_bar_exp_gain: dict[str, int], user_exp_bars: dict[str, "ExpBar"]):
+            exp_bar_exp_gain['HD'] = int (exp_bar_exp_gain['HD'] * (1 + 0.002 * upgrade_level * user_exp_bars['HD'].level))
+        
+        self.upgrades['hd_exp_gain_multiplier'] = (Upgrade(
+            id = "hd_exp_gain_multiplier",
+            name = "HD EXP Gain Multiplier", 
+            description = "+0.2% HD EXP gain / HD level, per upgrade level (additive)",
+            max_level = 5,
+            cost_currency_unit = "taiko_tokens", 
+            cost = lambda level: 200 * (level ** 2),
+            effect = BuffEffect.HD_EXP_GAIN,
+            effect_type = BuffEffectType.MULTIPLICATIVE,
+            effect_impl = hd_exp_gain_multiplier_effect,
+        ))
+        
+        def hr_exp_gain_multiplier_effect(upgrade_level: int, exp_bar_exp_gain: dict[str, int], user_exp_bars: dict[str, "ExpBar"]):
+            exp_bar_exp_gain['HR'] = int (exp_bar_exp_gain['HR'] * (1 + 0.002 * upgrade_level * user_exp_bars['HR'].level))
+        
+        self.upgrades['hr_exp_gain_multiplier'] = (Upgrade(
+            id = "hr_exp_gain_multiplier",
+            name = "HR EXP Gain Multiplier", 
+            description = "+0.2% HR EXP gain / HR level, per upgrade level (additive)",
+            max_level = 5,
+            cost_currency_unit = "taiko_tokens", 
+            cost = lambda level: 200 * (level ** 2),
+            effect = BuffEffect.HR_EXP_GAIN,
+            effect_type = BuffEffectType.MULTIPLICATIVE,
+            effect_impl = hr_exp_gain_multiplier_effect,
+        ))
+        
+        def dt_exp_gain_multiplier_effect(upgrade_level: int, exp_bar_exp_gain: dict[str, int], user_exp_bars: dict[str, "ExpBar"]):
+            exp_bar_exp_gain['DT'] = int (exp_bar_exp_gain['DT'] * (1 + 0.002 * upgrade_level * user_exp_bars['DT'].level))
+        
+        self.upgrades['dt_exp_gain_multiplier'] = (Upgrade(
+            id = "dt_exp_gain_multiplier",
+            name = "DT EXP Gain Multiplier", 
+            description = "+0.2% DT EXP gain / DT level, per upgrade level (additive)",
+            max_level = 5,
+            cost_currency_unit = "taiko_tokens", 
+            cost = lambda level: 200 * (level ** 2),
+            effect = BuffEffect.DT_EXP_GAIN,
+            effect_type = BuffEffectType.MULTIPLICATIVE,
+            effect_impl = dt_exp_gain_multiplier_effect,
+        ))
+        
+        def ht_exp_gain_multiplier_effect(upgrade_level: int, exp_bar_exp_gain: dict[str, int], user_exp_bars: dict[str, "ExpBar"]):
+            exp_bar_exp_gain['HT'] = int (exp_bar_exp_gain['HT'] * (1 + 0.002 * upgrade_level * user_exp_bars['HT'].level))
+        
+        self.upgrades['ht_exp_gain_multiplier'] = (Upgrade(
+            id = "ht_exp_gain_multiplier",
+            name = "HT EXP Gain Multiplier", 
+            description = "+0.2% HT EXP gain / HT level, per upgrade level (additive)",
+            max_level = 5,
+            cost_currency_unit = "taiko_tokens", 
+            cost = lambda level: 200 * (level ** 2),
+            effect = BuffEffect.HT_EXP_GAIN,
+            effect_type = BuffEffectType.MULTIPLICATIVE,
+            effect_impl = ht_exp_gain_multiplier_effect,
+        ))
+    
         def tt_gain_efficiency_effect(upgrade_level: int, score: "Score", currency_gain: dict[str, int]):
             currency_gain['taiko_tokens'] = score.note_hits // (NOTE_HITS_REQUIRED_PER_TAIKO_TOKEN - upgrade_level)
     
         self.upgrades['tt_gain_efficiency'] = (Upgrade(
             id = "tt_gain_efficiency",
-            name = "Taiko Token Gain Efficiency", 
+            name = "Taiko Token Gain Efficiency",
             description = f"-1 note hit needed to gain a Taiko Token. You gain a token every {NOTE_HITS_REQUIRED_PER_TAIKO_TOKEN} hits by default",
             max_level = 20,
             cost_currency_unit = "taiko_tokens", 
