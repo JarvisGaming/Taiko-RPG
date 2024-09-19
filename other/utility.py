eba06e5da8253fb4818adb7815ad47619bac55ab
JarvisGaming <jarvisgaming40@gmail.com>
2024-09-19 16:05:45 +0800

    Lower db backup frequency

--- other/utility.py
+++ other/utility.py
@@ -49,7 +49,7 @@ async def regularly_refresh_osu_api_access_token():
         dotenv.set_key(dotenv_path="./data/sensitive.env", key_to_set="OSU_API_ACCESS_TOKEN", value_to_set=json_file['access_token'])  # Global
         print(f"{datetime.datetime.now()}: Access token refreshed", flush=True)
 
-@tasks.loop(hours=1)
+@tasks.loop(hours=3)
 async def regularly_backup_database():
     google_auth.Refresh()  # Refreshes access token, which expires after 1 hour
 
